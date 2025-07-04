832a43b88c223484de3f4b1239ba67526151c953
yusef arif <yusefarif616@gmail.com>
2025-07-04 11:21:00 +0300

    fix error

--- src/services/lessonAPI.js
+++ src/services/lessonAPI.js
@@ -3,31 +3,31 @@ import axios from "axios";
 import { axiosErrorHandler } from "@/lib/utils";
 
 export async function getLessonById(id) {
+  const token = localStorage.getItem("token");
   try {
     const res = await axios.get(
       `https://edu-master-delta.vercel.app/lesson/${id}`,
       {
         headers: {
+          token,
           "Content-Type": "application/json",
-          token:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1obXdkYmhqdDMwN0BnbWFpbC5jb20iLCJfaWQiOiI2ODUzMDNkYWNhZTIyN2VkMjM0MWQ3ZGQiLCJpYXQiOjE3NTE0NjQ0ODksImV4cCI6MTc1MTU1MDg4OX0.x2RuausRJ9aYLBXIbeVhrpU1UhoQ2c-6D7JRJ26cKmM",
         },
       }
     );
 
-    if (!res.data.success) {
-      throw new Error("Failed to fetch lessons");
-    }
-
     return res.data.data;
   } catch (error) {
-    console.error("Error fetching lessons: ", error);
-    // throw new Error("Failed to fetch lessons");
-    throw new Error(axiosErrorHandler(error, "Failed to fetch lessons"));
+    if (error.status === 403) {
+      return null;
+    }
+    console.error("Error fetching lesson: ", error);
+    throw new Error(axiosErrorHandler(error, "Failed to fetch lesson"));
   }
 }
 
 export async function payLesson(id) {
+  const token = localStorage.getItem("token");
+
   try {
     const res = await axios.post(
       `https://edu-master-delta.vercel.app/lesson/pay/${id}`,
@@ -35,13 +35,13 @@ export async function payLesson(id) {
       {
         headers: {
           "Content-Type": "application/json",
-          token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1obXdkYmhqdDMwN0BnbWFpbC5jb20iLCJfaWQiOiI2ODUzMDNkYWNhZTIyN2VkMjM0MWQ3ZGQiLCJpYXQiOjE3NTE0NjQ0ODksImV4cCI6MTc1MTU1MDg4OX0.x2RuausRJ9aYLBXIbeVhrpU1UhoQ2c-6D7JRJ26cKmM",
+          token,
         },
       }
     );
 
     if (!res.data.success) {
-      throw new Error("Failed to fetch lessons");
+      throw new Error("Failed to fetch lesson");
     }
     return res.data;
   } catch (error) {
