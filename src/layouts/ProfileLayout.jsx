d62705196e4528bf2299cdfafc7bed45e39f2027
Rawda Ashor <141156812+rawda2@users.noreply.github.com>
2025-07-12 12:37:35 +0300

    Update ProfileLayout.jsx

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 export default function ProfileLayout() {
   return (
     <div className="flex min-h-screen">
-      {/* <Sidebar /> */}
+      <Sidebar />
       <main className="flex-1">
         <Outlet />
       </main>
